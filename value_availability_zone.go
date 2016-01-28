992d8632f08ba4e594c4d464a150808bb6531c10
James Gregory <james@jagregory.com>
2016-01-28 13:12:51 +1100

    Add all availability zones

--- value_availability_zone.go
+++ value_availability_zone.go
@@ -1,3 +1,31 @@
 package main
 
-var AvailabilityZone = EnumSchema("ap-southeast-2f", "ap-southeast-2g")
+var AvailabilityZone = EnumSchema(
+	"ap-northeast-1a",
+	"ap-northeast-1b",
+	"ap-northeast-1c",
+	"ap-northeast-2a",
+	"ap-northeast-2c",
+	"ap-southeast-1a",
+	"ap-southeast-1b",
+	"ap-southeast-2a",
+	"ap-southeast-2b",
+	"eu-central-1a",
+	"eu-central-1b",
+	"eu-west-1a",
+	"eu-west-1b",
+	"eu-west-1c",
+	"sa-east-1a",
+	"sa-east-1b",
+	"sa-east-1c",
+	"us-east-1a",
+	"us-east-1b",
+	"us-east-1c",
+	"us-east-1d",
+	"us-east-1e",
+	"us-west-1a",
+	"us-west-1c",
+	"us-west-2a",
+	"us-west-2b",
+	"us-west-2c",
+)
