418adc582979570c33a8b1197084aca09aa64314
James Gregory <james@jagregory.com>
2016-02-25 20:35:19 +1100

    Add supported functions to Fn::FindInMap

--- schema/if_find_in_map.go
+++ schema/if_find_in_map.go
@@ -5,57 +5,99 @@ import (
 	"github.com/jagregory/cfval/reporting"
 )
 
-func validateFindInMap(findInMap parse.IntrinsicFunction, ctx PropertyContext) (reporting.ValidateResult, reporting.Reports) {
-	argsValue, found := findInMap.UnderlyingMap["Fn::FindInMap"]
-	if !found || argsValue == nil {
+// see: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference-findinmap.html
+func validateFindInMap(builtin parse.IntrinsicFunction, ctx PropertyContext) (reporting.ValidateResult, reporting.Reports) {
+	value, found := builtin.UnderlyingMap["Fn::FindInMap"]
+	if !found || value == nil {
 		return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Missing \"Fn::FindInMap\" key")}
 	}
 
-	argsArray, ok := argsValue.([]interface{})
+	args, ok := value.([]interface{})
 	if !ok {
-		return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Invalid \"Fn::FindInMap\" key: %s", argsArray)}
+		return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Invalid \"Fn::FindInMap\" key: %s", args)}
 	}
 
-	if len(findInMap.UnderlyingMap) > 1 {
-		return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Unexpected extra keys: %s", keysExcept(findInMap.UnderlyingMap, "Fn::FindInMap"))}
+	if len(builtin.UnderlyingMap) > 1 {
+		return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Unexpected extra keys: %s", keysExcept(builtin.UnderlyingMap, "Fn::FindInMap"))}
 	}
 
-	if len(argsArray) != 3 {
-		return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Options has wrong number of items, expected: 3, actual: %d", len(argsArray))}
+	if len(args) != 3 {
+		return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Options has wrong number of items, expected: 3, actual: %d", len(args))}
 	}
 
-	findInMapItemContext := NewPropertyContext(ctx, Schema{Type: ValueString})
+	reports := make(reporting.Reports, 0, 10)
 
-	mapName := argsArray[0]
-	_, mapNameIsString := mapName.(string)
-	if _, errs := ValueString.Validate(mapName, PropertyContextAdd(findInMapItemContext, "0")); errs != nil {
-		return reporting.ValidateAbort, errs
+	mapName := args[0]
+	if _, errs := validateFindInMapMapName(builtin, mapName, PropertyContextAdd(ctx, "MapName")); errs != nil {
+		reports = append(reports, errs...)
 	}
 
-	if mapNameIsString {
-		// map name is a string, so we can do some further interrogation
-		// TODO: lookup whether MapName is a valid Map
+	topLevelKey := args[1]
+	if _, errs := validateFindInMapTopLevelKey(builtin, mapName, topLevelKey, PropertyContextAdd(ctx, "TopLevelKey")); errs != nil {
+		reports = append(reports, errs...)
 	}
 
-	topLevelKey := argsArray[1]
-	_, topLevelKeyIsString := topLevelKey.(string)
-	if _, errs := ValueString.Validate(topLevelKey, PropertyContextAdd(findInMapItemContext, "1")); errs != nil {
-		return reporting.ValidateAbort, errs
+	secondLevelKey := args[2]
+	if _, errs := validateFindInMapSecondLevelKey(builtin, mapName, topLevelKey, secondLevelKey, PropertyContextAdd(ctx, "SecondLevelKey")); errs != nil {
+		reports = append(reports, errs...)
 	}
 
-	if mapNameIsString && topLevelKeyIsString {
-		// TODO: lookup whether topLevelKey is in mapName
+	return reporting.ValidateAbort, reporting.Safe(reports)
+}
+
+func validateFindInMapMapName(builtin parse.IntrinsicFunction, mapName interface{}, ctx PropertyContext) (reporting.ValidateResult, reporting.Reports) {
+	if mapName == nil {
+		return reporting.ValidateOK, reporting.Reports{reporting.NewFailure(ctx, "Cannot be null")}
 	}
 
-	secondLevelKey := argsArray[2]
-	_, secondLevelKeyIsString := secondLevelKey.(string)
-	if _, errs := ValueString.Validate(secondLevelKey, PropertyContextAdd(findInMapItemContext, "2")); errs != nil {
-		return reporting.ValidateAbort, errs
+	switch t := mapName.(type) {
+	case string:
+		// TODO: validate actual map exists
+		return reporting.ValidateOK, nil
+	case parse.IntrinsicFunction:
+		return ValidateIntrinsicFunctions(t, ctx, SupportedFunctions{
+			parse.FnFindInMap: true,
+			parse.FnRef:       true,
+		})
 	}
 
-	if mapNameIsString && topLevelKeyIsString && secondLevelKeyIsString {
-		// TODO: lookup whether secondLevelKeyIsString is in topLevelKey
+	return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Invalid MapName: %v", mapName)}
+}
+
+func validateFindInMapTopLevelKey(builtin parse.IntrinsicFunction, mapName, topLevelKey interface{}, ctx PropertyContext) (reporting.ValidateResult, reporting.Reports) {
+	if topLevelKey == nil {
+		return reporting.ValidateOK, reporting.Reports{reporting.NewFailure(ctx, "Cannot be null")}
 	}
 
-	return reporting.ValidateAbort, nil
+	switch t := topLevelKey.(type) {
+	case string:
+		// TODO: validate actual map top level key exists, if possible
+		return reporting.ValidateOK, nil
+	case parse.IntrinsicFunction:
+		return ValidateIntrinsicFunctions(t, ctx, SupportedFunctions{
+			parse.FnFindInMap: true,
+			parse.FnRef:       true,
+		})
+	}
+
+	return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Invalid TopLevelKey: %v", topLevelKey)}
+}
+
+func validateFindInMapSecondLevelKey(builtin parse.IntrinsicFunction, mapName, topLevelKey, secondLevelKey interface{}, ctx PropertyContext) (reporting.ValidateResult, reporting.Reports) {
+	if secondLevelKey == nil {
+		return reporting.ValidateOK, reporting.Reports{reporting.NewFailure(ctx, "Cannot be null")}
+	}
+
+	switch t := secondLevelKey.(type) {
+	case string:
+		// TODO: validate actual map second level key exists, if possible
+		return reporting.ValidateOK, nil
+	case parse.IntrinsicFunction:
+		return ValidateIntrinsicFunctions(t, ctx, SupportedFunctions{
+			parse.FnFindInMap: true,
+			parse.FnRef:       true,
+		})
+	}
+
+	return reporting.ValidateAbort, reporting.Reports{reporting.NewFailure(ctx, "Invalid SecondLevelKey: %v", secondLevelKey)}
 }
